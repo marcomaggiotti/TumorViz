92574843b03cc37b4800810cd13ad53add85d213
maorvelous <ing.marco.maggiotti@hotmail.it>
2020-01-08 22:47:17 +0100

    working scripts for SVS, MRXS Slides

---
+++ GroundTruthBlendOpenCv_ver2.py
@@ -0,0 +1,122 @@
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import scipy.signal
+import argparse
+from PIL import Image, ImageDraw
+from PIL import ImagePath
+from matplotlib.collections import PolyCollection
+import matplotlib.patches as pat
+import sklearn.feature_extraction.image
+import matplotlib.cm
+import torch
+from skimage.external.tifffile import imread
+from torchvision.models import DenseNet
+from tqdm.autonotebook import tqdm
+from  skimage.color import rgb2gray
+from skimage import measure
+import skimage.color as color
+import skimage
+import openslide
+import PIL.Image
+
+size = 896, 385
+
+fname=r"/home/maorvelous/Documents/Lab/deepLearning/data/mib1/BRCA03_MIB1.mrxs"
+openSlideReader  = openslide.OpenSlide(fname)
+
+#add mask creation which skips parts of image
+mask_level = 4
+img_ground_Openslide = openSlideReader.read_region((0, 0), mask_level, openSlideReader.level_dimensions[mask_level])
+img_ground_Openslide_array = np.asarray(img_ground_Openslide)[:, :, 0:3]
+plt.imshow(img_ground_Openslide)
+plt.show()
+imgRegions = imread('/home/maorvelous/Documents/Lab/deepLearning/PytorchDigitalPathology/BracCedric/03.tif', key = 0)
+hsv=cv2.cvtColor(imgRegions,cv2.COLOR_BGR2YCrCb)
+
+darkGreen_lo=np.array([60,60,60])
+darkGreen_hi=np.array([180,180,180])
+
+mask=cv2.inRange(hsv,darkGreen_lo,darkGreen_hi)
+imgRegions[mask>0]=(0,0,0)
+im = Image.fromarray(imgRegions)
+im.save("./orig_3.jpg")
+imgg = rgb2gray(imgRegions)
+im = Image.fromarray(imgg)
+contours = measure.find_contours(im, 0.1)
+
+img_ground_Openslide_array = np.asanyarray(img_ground_Openslide_array)
+im1 = Image.fromarray(img_ground_Openslide_array)
+im1.save("./tumor_3.jpg")
+
+def drawShape(img, coordinates, color):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgage = skimage.color.gray2rgb(gray)
+    # Make sure the coordinates are expressed as integers
+    coordinates = coordinates.astype(int)
+    imgage[coordinates[:, 0], coordinates[:, 1]] = color
+    return imgage
+
+for contour in contours:
+    imgFinal = drawShape(imgRegions, contour, [255, 255, 0])
+# Grayscale
+
+gray = cv2.cvtColor(imgRegions, cv2.COLOR_BGR2GRAY)
+
+# Find Canny edges
+edged = cv2.Canny(gray, 30, 200)
+print(gray.shape)
+
+cv2.waitKey(0)
+contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+print('Original Dimensions : ',edged.shape)
+
+scale_percent = 40 # percent of original size
+width = int(edged.shape[1] * scale_percent / 100)
+height = int(edged.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+# resize image
+resized = cv2.resize(edged, dim, interpolation = cv2.INTER_AREA)
+
+contoursGroundArray = cv2.drawContours(img_ground_Openslide_array, contours, -1, (0, 255, 0), 3)
+print(contoursGroundArray)
+
+new = cv2.imread('/home/maorvelous/Documents/Lab/deepLearning/PytorchDigitalPathology/BracCedric/tumor_3.jpg')
+new = cv2.resize(new, imgRegions.shape[1::-1])
+dst = cv2.addWeighted(new, 0.9, imgRegions, 0.4, 0.0)
+
+def nothing(x):
+    pass
+
+# Create a black image, a window
+img = np.zeros((300,512,3), np.uint8)
+cv2.namedWindow('dst')
+
+# create trackbars for color change
+cv2.createTrackbar('Original','dst',1,100,nothing)
+cv2.createTrackbar('Tumor','dst',1,100,nothing)
+cv2.createTrackbar('Immune','dst',0,255,nothing)
+
+dst = cv2.resize(dst, dim, interpolation = cv2.INTER_AREA)
+
+while(1):
+    cv2.imshow('image',dst)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    # get current positions of four trackbars
+    first = cv2.getTrackbarPos('Original','dst')
+    second = cv2.getTrackbarPos('Tumor','dst')
+
+    b = cv2.getTrackbarPos('B','image')
+
+    first = first/50
+    second = second/50
+    dst = cv2.addWeighted(new, first, imgRegions, second, 0.0)
+    dst = cv2.resize(dst, dim, interpolation = cv2.INTER_AREA)
+
+cv2.destroyAllWindows()
---
+++ OpenLargeImageSVS.py
@@ -0,0 +1,161 @@
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import scipy.signal
+import argparse
+from PIL import Image, ImageDraw
+from PIL import ImagePath
+from matplotlib.collections import PolyCollection
+import matplotlib.patches as pat
+import sklearn.feature_extraction.image
+import matplotlib.cm
+import torch
+from skimage.external.tifffile import imread
+from torchvision.models import DenseNet
+from tqdm.autonotebook import tqdm
+from  skimage.color import rgb2gray
+from skimage import measure
+import skimage.color as color
+import skimage
+import openslide
+import PIL.Image
+import py_wsi
+import py_wsi.imagepy_toolkit as tk
+import py_wsi.dataset as ds
+from torchviz import make_dot
+from openslide import open_slide
+from openslide.deepzoom import DeepZoomGenerator
+
+parser = argparse.ArgumentParser(description='Make output for entire image using Unet')
+parser.add_argument('input_pattern',
+                    help="input filename pattern. try: *.png, or tsv file containing list of files to analyze",nargs="*")
+
+parser.add_argument('-p', '--patchsize', help="patchsize, default 256", default=256, type=int)
+parser.add_argument('-s', '--batchsize', help="batchsize for controlling GPU memory usage, default 10", default=10, type=int)
+parser.add_argument('-o', '--outdir', help="outputdir, default ./output/", default="./output/", type=str)
+parser.add_argument('-r', '--resize', help="resize factor 1=1x, 2=2x, .5 = .5x", default=1, type=float)
+parser.add_argument('-m', '--model', help="model", default="best_model.pth", type=str)
+parser.add_argument('-i', '--gpuid', help="id of gpu to use", default=0, type=int)
+parser.add_argument('-f', '--force', help="force regeneration of output even if it exists", default=False,
+                    action="store_true")
+parser.add_argument('-b', '--basepath',
+                    help="base path to add to file names, helps when producing data using tsv file as input",
+                    default="", type=str)
+
+#tk.show_images([new_tile, new_tile2, new_tile3,new_tile4, new_tile5, new_tile6, new_tile7, new_tile8, new_tile9], 3, 9)
+args = parser.parse_args(["-mbrca1_no_58_densenet_best_model_2_8_8_8_8_8.pth"])
+
+device = torch.device(args.gpuid if args.gpuid!=-2 and torch.cuda.is_available() else 'cpu')
+checkpoint = torch.load("brca1_no_58_densenet_best_model_2_8_8_8_8_8.pth", map_location=lambda storage, loc: storage) #load checkpoint to CPU and then put to device https://discuss.pytorch.org/t/saving-and-loading-torch-models-on-2-machines-with-different-number-of-gpu-devices/6666
+
+model = DenseNet(growth_rate=checkpoint["growth_rate"], block_config=checkpoint["block_config"],
+                 num_init_features=checkpoint["num_init_features"], bn_size=checkpoint["bn_size"],
+                 drop_rate=checkpoint["drop_rate"], num_classes=checkpoint["num_classes"]).to(device)
+
+model.load_state_dict(checkpoint["model_dict"])
+model.eval()
+osh  = openslide.OpenSlide("/home/maorvelous/Documents/Lab/deepLearning/data/gdc/TCGA-OL-A5RW-01Z-00-DX1.E16DE8EE-31AF-4EAF-A85F-DB3E3E2C3BFF.svs")
+cmap= matplotlib.cm.tab10
+
+#print(tiles._z_t_downsample)
+#{patch_size}_{batch_size}_{level}_{mask_level}_{shape[0]}_{shape[1]}_{stride_size}_
+#{tile_size}_{downsamples_level}_{cycle_step}
+
+patch_size = 8 #if patch is less than 32 fail
+batch_size = 64 #should be a power of 2
+level = 0
+mask_level = 1 # con la mask = 2 ValueError: could not broadcast input array from shape (16,16,3) into shape (15,16,3)
+
+stride_size= patch_size // 4
+tile_size = stride_size * 8 * 2
+tile_pad = patch_size - stride_size
+
+downsamples_level = round(osh.level_downsamples[level])
+
+cycle_step = round(tile_size * downsamples_level)
+nclasses = 3
+
+ds=int(downsamples_level)
+print(ds)
+
+size_slide = osh.level_dimensions[level]
+slide_origin = (0 , 0)
+
+img_resized = osh.read_region(slide_origin, mask_level, size_slide )
+
+plt.imshow(img_resized)
+plt.show()
+print("image size ", img_resized.size)
+shape = img_resized.size #osh.level_dimensions[level]
+
+img = img_resized #np.asarray(new_tile9)[:, :, 0:3] #osh.read_region((0, 0), mask_level, osh.level_dimensions[mask_level]) #
+imgg=rgb2gray(np.array(img))
+#imgg = img.convert('LA')
+
+mask=np.bitwise_and(imgg >0 ,imgg <200/255)
+shaperound=[((d//tile_size)+1)*tile_size for d in shape]
+
+def divide_batch(l, n):
+    for i in range(0, l.shape[0], n):
+        yield l[i:i + n,::]
+
+print("Patch Floor Division", patch_size//patch_size,patch_size//patch_size)
+npmm = np.zeros((shaperound[1]//stride_size,shaperound[0]//stride_size,3),dtype=np.uint8)
+
+#cycle_step = 8#round(tile_size * downsamples_level)
+y_space = range(0,osh.level_dimensions[0][1],round(tile_size * downsamples_level))
+x_space = range(0,osh.level_dimensions[0][0],round(tile_size * downsamples_level))
+
+print(range(0,img_resized.height,round(tile_size * downsamples_level)))
+print(range(0,img_resized.width,round(tile_size * downsamples_level)))
+
+plt.imshow(mask)
+plt.show()
+
+for y in tqdm(range(0, img_resized.height,cycle_step), desc="outer"):
+    for x in tqdm(range(0, img_resized.width,cycle_step), desc=f"innter {y}", leave=False):
+
+        maskx = int(x//downsamples_level)
+        masky = int(y//downsamples_level)
+
+        if(maskx>= mask.shape[1] or masky>= mask.shape[0] or not mask[masky,maskx]): #need to handle rounding error
+            #print("jump the model")
+            continue
+
+        #print(mask[masky,maskx])
+        #print("x",x, "y",y,"maskx",maskx, " masky", masky, " mask.shape[1]", mask.shape[1], " mask.shape[0]", mask.shape[0] , "tile_size", tile_size, " osh.level_downsamples[level]",  osh.level_downsamples[level] )
+
+        #coordinates = [[x,y], [x+tile_size+tile_pad , y],[x+tile_size+tile_pad,y+tile_size+tile_pad],[x,y+tile_size+tile_pad]]
+        patch_size//patch_size
+        output = np.zeros((0,nclasses, patch_size//patch_size,patch_size//patch_size ))
+        io = np.asarray(osh.read_region((x, y), 3, (tile_size+tile_pad,tile_size+tile_pad)))[:,:,0:3]
+
+        arr_out = sklearn.feature_extraction.image.extract_patches(io,(patch_size,patch_size,3),stride_size)
+        arr_out_shape = arr_out.shape
+        arr_out = arr_out.reshape(-1,patch_size,patch_size,3)
+
+        for batch_arr in divide_batch(arr_out,1):
+
+            arr_out_gpu = torch.from_numpy(batch_arr.transpose(0, 3, 1, 2) / 255).type('torch.FloatTensor').to(device)
+            #print("arr_out_gpu", arr_out_gpu)
+            output_batch = model(arr_out_gpu)
+            #make_dot(output_batch)
+            output_batch = output_batch.detach().cpu().numpy()
+            output_batch_color = cmap(output_batch.argmax(axis=1), alpha=None)[:,0:3]
+            output = np.append(output,output_batch_color[:,:,None,None],axis=0)
+
+        output = output.transpose((0, 2, 3, 1))
+        output = output.reshape(arr_out_shape[0],arr_out_shape[1],patch_size//patch_size,patch_size//patch_size,output.shape[3])
+
+        output=np.concatenate(np.concatenate(output,1),1)
+        #print(y//stride_size//ds,y//stride_size//ds+tile_size//stride_size,x//stride_size//ds,x//stride_size//ds+tile_size//stride_size)
+        npmm[y//stride_size//ds:y//stride_size//ds+tile_size//stride_size,x//stride_size//ds:x//stride_size//ds+tile_size//stride_size,:]=output*255
+
+from skimage.external.tifffile import TiffWriter
+with TiffWriter(f'/home/maorvelous/Documents/Lab/react/images/BRACA_{patch_size}_{batch_size}_{level}_{mask_level}_{shape[0]}_{shape[1]}_{stride_size}_{tile_size}_{downsamples_level}_{cycle_step}_svs.tif', bigtiff=True, imagej=True) as tif:
+    tif.save(npmm, compress=0, tile=(256,256) )
+
+image = Image.open(f'/home/maorvelous/Documents/Lab/react/images/TCGA-OL-A5RW-01Z-00-DX1.E16DE8EE-31AF-4EAF-A85F-DB3E3E2C3BFF_{patch_size}_{batch_size}_{level}_{mask_level}_{shape[0]}_{shape[1]}_{stride_size}_{tile_size}_{downsamples_level}_{cycle_step}_svs.tif')
+image.mode = 'I'
+image.point(lambda i:i*(1./256)).convert('L').save(f'/home/maorvelous/Documents/Lab/react/images/TCGA-OL-A5RW-01Z-00-DX1.E16DE8EE-31AF-4EAF-A85F-DB3E3E2C3BFF_{patch_size}_{batch_size}_{level}_{mask_level}_{shape[0]}_{shape[1]}_{stride_size}_{tile_size}_{downsamples_level}_{cycle_step}_svs.jpeg')
